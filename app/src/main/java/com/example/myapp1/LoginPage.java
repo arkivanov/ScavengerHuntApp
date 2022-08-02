df99e7d7e0c396ff25957e3da888b29addc1d324
annxkin1234 <77632880+annxkin1234@users.noreply.github.com>
2022-08-02 16:00:52 +0100

    initial commit

---
+++ app/src/main/java/com/example/myapp1/LoginPage.java
@@ -0,0 +1,55 @@
+package com.example.myapp1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class LoginPage extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton facebook,google,twitter;
+    float v = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.view_pager);
+        facebook = findViewById(R.id.fab_facebook);
+        google = findViewById(R.id.fab_google);
+        twitter = findViewById(R.id.fab_twitter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener((ViewPager.OnPageChangeListener) new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        facebook.setTranslationY(300);
+        google.setTranslationY(300);
+        twitter.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+        facebook.setAlpha(v);
+        google.setAlpha(v);
+        twitter.setAlpha(v);
+        tabLayout.setAlpha(v);
+
+        facebook.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapp1/SigninTabFragment.java
@@ -0,0 +1,42 @@
+package com.example.myapp1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class SigninTabFragment extends Fragment {
+
+    FloatingActionButton email,password,forgot,button;
+    float v = 0;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signin_fragment, container, false);
+
+            email = root.findViewById(R.id.email);
+            password = root.findViewById(R.id.password);
+            forgot = root.findViewById(R.id.fab_facebook);
+            button = root.findViewById(R.id.fab_google);
+
+            email.setTranslationY(300);
+            password.setTranslationY(300);
+            forgot.setTranslationY(300);
+            button.setTranslationY(300);
+
+            email.setAlpha(v);
+            password.setAlpha(v);
+            forgot.setAlpha(v);
+            button.setAlpha(v);
+
+            email.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+            password.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+            forgot.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+            button.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
+        return root;
+    }
+}
