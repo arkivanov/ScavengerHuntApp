815606a940c46c081c932ed66afd42cfb51723b5
annxkin1234 <77632880+annxkin1234@users.noreply.github.com>
2022-08-02 16:42:03 +0100

    working version

--- app/src/main/java/com/example/myapp1/SigninTabFragment.java
+++ app/src/main/java/com/example/myapp1/SigninTabFragment.java
@@ -5,13 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SigninTabFragment extends Fragment {
 
-    FloatingActionButton email,password,forgot,button;
+    FloatingActionButton button;
+    AppCompatEditText email,password;
     float v = 0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -19,23 +21,23 @@ public class SigninTabFragment extends Fragment {
 
             email = root.findViewById(R.id.email);
             password = root.findViewById(R.id.password);
-            forgot = root.findViewById(R.id.fab_facebook);
-            button = root.findViewById(R.id.fab_google);
+            //forgot = root.findViewById(R.id.forgot);
+            //button = root.findViewById(R.id.button);
 
             email.setTranslationY(300);
             password.setTranslationY(300);
-            forgot.setTranslationY(300);
-            button.setTranslationY(300);
+            //forgot.setTranslationY(300);
+            //button.setTranslationY(300);
 
             email.setAlpha(v);
             password.setAlpha(v);
-            forgot.setAlpha(v);
-            button.setAlpha(v);
+            //forgot.setAlpha(v);
+            //button.setAlpha(v);
 
             email.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
             password.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
-            forgot.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
-            button.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+            //forgot.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+            //button.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
 
         return root;
     }
